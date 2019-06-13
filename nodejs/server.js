a766176960d2503e00632ba1fa48363f8607dc35
Jeff St. Jean <jstjean3@uwo.ca>
2019-06-13 18:07:54 -0400

    Initial NodeJS files

---
+++ nodejs/server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const app = require('./app');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer();
+console.log('Listening on port ' + port);
+app.listen(port);
